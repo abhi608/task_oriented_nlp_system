eb1a5d52f31cca60eab325a0fa06a3fc1b3dd294
Aniket Maiti <aniket.maiti1996@gmail.com>
2018-04-20 05:04:15 +0530

    Added new model

---
+++ memn2n_src/main_model2.py
@@ -0,0 +1,209 @@
+from __future__ import division
+import torch
+import numpy as np
+from torch.autograd import Variable as Var
+
+dtype = torch.FloatTensor
+if torch.cuda.device_count() > 0:
+    dtype = torch.cuda.FloatTensor
+    print("Running on GPU")
+
+
+class MemN2NDialog(object):
+    """End-To-End Memory Network."""
+
+    def __init__(self, batch_size, vocab_size, candidate_size, sentence_size, candidates_vec,
+                 embedding_size=20, hops=3, learning_rate=1e-6, max_grad_norm=40.0, task_id=1):
+
+        # Constants
+        self._batch_size = batch_size
+        self._vocab_size = vocab_size
+        self._sentence_size = sentence_size
+        self._embedding_size = embedding_size
+        self._hops = hops
+        self._max_grad_norm = max_grad_norm
+        self._learn_rate = learning_rate
+        self._candidate_size = candidate_size
+        self._candidates = candidates_vec
+
+        # Weight matrices
+        self.q_embed = torch.nn.Embedding(self._vocab_size, self._embedding_size)
+        # self.c_embed = torch.nn.Embedding(self._vocab_size, self._embedding_size)
+        self.H = Var(torch.randn(self._embedding_size,
+                                 self._embedding_size).type(dtype), requires_grad=True)
+        self.out_embed = torch.nn.Embedding(self._vocab_size, self._embedding_size)
+
+        # Functions
+        self.softmax = torch.nn.Softmax(dim=0)
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+
+    def single_pass(self, stories, queries):
+        # Get Embeddings
+        q_emb = self.q_embed(queries)  # Get vocab embedding
+        u = torch.sum(q_emb, 1)      # Reduce sentence size
+
+        # Pass through Memory Network
+        for _ in range(self._hops):
+            m_emb = self.q_embed(stories)  # memory vectors
+            m = torch.sum(m_emb, 2)        # Reduce sentence size
+
+            # Calculate probabilities
+            p = self.softmax(m.bmm(u.view(self._batch_size, self._embedding_size, 1)))
+
+            # # Uncomment when calculating separate answer embedding
+            # c_emb = self.c_embed(stories)  # possible outputs
+            # c = c_emb.sum(2)
+
+            # Calculate possible output
+            p_temp = p.view(self._batch_size, 1, self._embedding_size)
+            o = p_temp.bmm(m).view(self._batch_size, self._embedding_size)
+
+            # Update next input
+            u = self.u.matmul(self.H) + o
+
+        # Get prediction
+        candidates_emb = self.out_embed(self._candidates)
+        candidates_emb = candidates_emb.sum(1)  # reducing sentence size
+        a_pred = u.matmul(candidates_emb.transpose())
+
+        return a_pred
+
+    def batch_train(self, stories, queries, answers):
+        a_pred = self.single_pass(stories, queries)
+
+        loss = self.loss_fn(a_pred, answers)
+        loss = loss.sum()
+
+        # Backprop and update weights
+        loss.backward()
+        self.update_weights()
+
+        return float(loss.data)
+
+    def test(self, stories, queries, answers):
+        a_pred = self.single_pass(stories, queries)
+        assert len(a_pred) == len(answers)
+
+        loss = self.loss_fn(a_pred, answers)
+        loss = loss.sum()
+
+        acc = 0
+        for b in range(len(a_pred)):
+            pred = np.argmax(a_pred[b].data.numpy())
+            actual = np.argmax(answers[b].data.numpy())
+            if pred == actual:
+                acc += 1
+        acc /= len(a_pred)
+
+        return acc, loss
+
+    def predict(self, story, query):
+        a_pred = self.single_pass([story], [query])
+
+        pred_index = np.argmax(a_pred[0].data.numpy())
+        pred_out = self._candidates[pred_index]
+
+        return pred_out
+
+    def update_weights(self):
+        # print self.A.grad.data
+        self.q_embed.weight.data -= self._learn_rate * self.q_embed.weight.grad.data
+        self.H.data -= self._learn_rate * self.H.grad.data
+        # self.c_embed.weight.data -= self._learn_rate * self.c_embed.weight.grad.data
+        self.out_embed.weight.data -= self._learn_rate * self.out_embed.weight.grad.data
+
+        # Manually zero the gradients after updating weights
+        self.q_embed.weight.grad.data.zero_()
+        self.H.grad.data.zero_()
+        # self.c_embed.weight.grad.data.zero_()
+        self.out_embed.weight.grad.data.zero_()
+
+    def save_weights(self, filename='model_weights.tar'):
+        weights = {'q_embed': self.q_embed, 'H': self.H, 'out_embed': self.out_embed}
+        torch.save(weights, filename)
+
+    def load_weights(self, filename='model_weights.tar'):
+        weights = torch.load(filename)
+        self.q_embed = weights['q_embed']
+        self.H = weights['H']
+        # self.c_embed = weights['c_embed']
+        self.out_embed = weights['out_embed']
+
+
+class MemN2NDialog_2(MemN2NDialog):
+    """End-To-End Memory Network."""
+
+    def __init__(self, batch_size, vocab_size, candidate_size, sentence_size, candidates_vec,
+                 embedding_size=20, hops=3, learning_rate=1e-6, max_grad_norm=40.0, task_id=1):
+        # Constants
+        self._batch_size = batch_size
+        self._vocab_size = vocab_size
+        self._sentence_size = sentence_size
+        self._embedding_size = embedding_size
+        self._hops = hops
+        self._max_grad_norm = max_grad_norm
+        self._learn_rate = learning_rate
+        self._candidate_size = candidate_size
+        self._candidates = candidates_vec
+
+        # Weight matrices
+        self.q_embed = torch.nn.Embedding(self._vocab_size, self._embedding_size)
+        self.c_embed = []
+        for _ in range(hops):
+            self.C.append(torch.nn.Embedding(self._vocab_size, self._embedding_size))
+        self.out_embed = torch.nn.Embedding(self._vocab_size, self._embedding_size)
+
+        # Functions
+        self.softmax = torch.nn.Softmax(dim=0)
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+
+    def single_pass(self, stories, queries):
+        # Get Embeddings
+        u = self.q_embed(queries)  # Get vocab embedding
+        u = torch.sum(u, 1)        # Reduce sentence size
+        m = self.q_embed(stories)  # Get memory vectors
+        m = torch.sum(m, 2)        # Reduce sentence size
+
+        # Pass through Memory Network
+        for hop in range(self._hops):
+            c = self.C[hop](stories)  # possible outputs
+            c = torch.sum(c, 2)
+
+            # Calculate probabilities
+            p = self.softmax(m.bmm(u.view(self._batch_size, self._embedding_size, 1)))
+
+            # Calculate possible output
+            p_temp = p.view(self._batch_size, 1, self._embedding_size)
+            o = p_temp.bmm(c).view(self._batch_size, self._embedding_size)
+
+            # Update next input
+            u = u + o
+            m = c
+
+        # Get prediction
+        candidates_emb = self.out_embed(self._candidates)
+        candidates_emb = candidates_emb.sum(1)  # reducing sentence size
+        a_pred = u.matmul(candidates_emb.transpose())
+
+        return a_pred
+
+    def update_weights(self):
+        self.q_embed.weight.data -= self._learn_rate * self.q_embed.weight.grad.data
+        self.q_embed.weight.grad.data.zero_()
+
+        for hop in range(self._hops):
+            self.C[hop].weight.data -= self._learn_rate * self.C[hop].weight.grad.data
+            self.C[hop].weight.grad.data.zero_()
+
+        self.out_embed.weight.data -= self._learn_rate * self.out_embed.weight.grad.data
+        self.out_embed.weight.grad.data.zero_()
+
+    def save_weights(self, filename='model_weights.tar'):
+        weights = {'q_embed': self.q_embed, 'C': self.C, 'out_embed': self.out_embed}
+        torch.save(weights, filename)
+
+    def load_weights(self, filename='model_weights.tar'):
+        weights = torch.load(filename)
+        self.q_embed = weights['q_embed']
+        self.C = weights['C']
+        self.out_embed = weights['out_embed']
---
+++ memn2n_src/train2.py
@@ -0,0 +1,170 @@
+from __future__ import absolute_import
+import os
+import json
+import argparse
+import numpy as np
+from DataLoader import CDATA
+from main_model2 import MemN2NDialog, MemN2NDialog_2
+from data_utils import load_candidates, load_dialog_task, vectorize_candidates
+from matplotlib import pyplot as plt
+
+class chatBot(object):
+    def __init__(self, data_dir, model_dir, task_id, isInteractive=True, OOV=False,
+                 memory_size=50, random_state=None, batch_size=32, learning_rate=0.001, epsilon=1e-8,
+                 max_grad_norm=40.0, evaluation_interval=10, hops=3, epochs=200, embedding_size=20, save_model=10,
+                 checkpoint_path='./models'):
+        self.data_dir = data_dir
+        self.task_id = task_id
+        self.model_dir = model_dir
+        self.isInteractive = isInteractive
+        self.OOV = OOV
+        self.memory_size = memory_size
+        self.random_state = random_state
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
+        self.max_grad_norm = max_grad_norm
+        self.evaluation_interval = evaluation_interval
+        self.hops = hops
+        self.epochs = epochs
+        self.embedding_size = embedding_size
+        self.save_model = save_model
+        self.checkpoint_path = checkpoint_path
+
+        self.losses = []
+
+        self.train_dataset = CDATA(data_dir=self.data_dir, task_id=self.task_id, memory_size=self.memory_size,
+                                   train=0, batch_size=self.batch_size, story_size=self.max_story_size)  # 0->train, 1->validate, 2->test
+        self.model = MemN2NDialog(batch_size=self.batch_size, vocab_size=self.train_dataset.getParam('vocab_size'),
+                                  candidate_size=self.train_dataset.getParam('candidate_sentence_size'), sentence_size=self.train_dataset.getParam('sentence_size'),
+                                  candidates_vec=self.train_dataset.getParam('candidates_vec'), embedding_size=self.embedding_size, hops=self.hops,
+                                  learning_rate=self.learning_rate, max_grad_norm=self.max_grad_norm, task_id=self.task_id)
+        # criterion = nn.CrossEntropyLoss()
+        # optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+    def train(self):
+        trainS, trainQ, trainA = self.train_dataset.getData()
+        assert len(trainS) == len(trainQ) and len(trainQ) == len(trainA)
+        n_train = len(trainS)
+        batches = zip(range(0, n_train - self.batch_size, self.batch_size),
+                      range(self.batch_size, n_train, self.batch_size))
+        batches = [(start, end) for start, end in batches]
+
+        for epoch in range(self.epochs):
+            print('\nepoch :', epoch)
+            np.random.shuffle(batches)
+            loss = 0.0
+            for start, end in batches:
+                s = trainS[start:end]
+                q = trainQ[start:end]
+                a = trainA[start:end]
+                loss += self.model.batch_train(s, q, a)
+
+            self.losses.append(loss)
+            print('loss = ', loss / n_train)
+
+            if epoch % self.save_model == 0:
+                fname = 'scratch_models/task{0}_epoch{1}_weights.tar'.format(self.task_id, epoch)
+                self.model.save_weights(filename=fname)
+
+    def test(self, data_type):
+        print("\nSTARTED TESTING")
+        dataset = CDATA(data_dir=self.data_dir, task_id=self.task_id, memory_size=self.memory_size,
+                        train=data_type, batch_size=self.batch_size)  # 0->train, 1->validate, 2->test
+        testS, testQ, testA = dataset.getData()
+        assert len(testS) == len(testQ) and len(testQ) == len(testA)
+        n_test = len(testS)
+
+        fname = 'scratch_models/task{0}_epoch{1}_weights.tar'.format(self.task_id)
+        self.model.load_weights(filename=fname)
+
+        acc, loss = self.model.test(testS, testQ, testA)
+        print('Accuracy = ', acc)
+
+    def build_vocab(self, data, candidates):
+        vocab = reduce(lambda x, y: x | y, (set(
+            list(chain.from_iterable(s)) + q) for s, q, a in data))
+        vocab |= reduce(lambda x, y: x | y, (set(candidate)
+                                             for candidate in candidates))
+        vocab = sorted(vocab)
+        self.word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+        self.max_story_size = max(map(len, (s for s, _, _ in data)))
+        mean_story_size = int(np.mean([len(s) for s, _, _ in data]))
+        self.sentence_size = max(
+            map(len, chain.from_iterable(s for s, _, _ in data)))
+        self.candidate_sentence_size = max(map(len, candidates))
+        query_size = max(map(len, (q for _, q, _ in data)))
+        self.memory_size = min(self.memory_size, max_story_size)
+        self.vocab_size = len(self.word_idx) + 1  # +1 for nil word
+        self.sentence_size = max(
+            query_size, self.sentence_size)  # for the position
+        # params
+        print("vocab size:", self.vocab_size)
+        print("Longest sentence length", self.sentence_size)
+        print("Longest candidate sentence length",
+              self.candidate_sentence_size)
+        print("Longest story length", self.max_story_size)
+        print("Average story length", mean_story_size)
+
+
+def main(params):
+    model_dir = "task" + str(params['task_id']) + "_" + params['model_dir']
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    chatbot = chatBot(data_dir=params['data_dir'], model_dir=model_dir, task_id=params['task_id'], isInteractive=params['interactive'], OOV=params['OOV'], memory_size=params['memory_size'], random_state=params['random_state'], batch_size=params['batch_size'],
+                      learning_rate=params['learning_rate'], epsilon=params['epsilon'], max_grad_norm=params['max_grad_norm'], evaluation_interval=params['evaluation_interval'], hops=params['hops'], epochs=params['epochs'], embedding_size=params['embedding_size'],
+                      save_model=params['save_model'], checkpoint_path=params['checkpoint_path'])
+    if params['train']:
+        chatbot.train()
+        # Plot Losses
+        plt.plot(chatbot.losses)
+        plt.x_label('Epochs')
+        plt.y_label('Losses')
+        plt.show()
+        plt.savefig('scratch_models/task{0}_epochs{1}_plot.png'.format(chatbot.task_id, chatbot.epochs))
+    else:
+        chatbot.test(0)
+        chatbot.test(1)
+        chatbot.test(2)
+    # chatbot.test()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--learning_rate', default=0.001, type=float,
+                        help='Learning rate for Adam Optimizer')
+    parser.add_argument('--epsilon', default=1e-8, type=float,
+                        help='Epsilon value for Adam Optimizer')
+    parser.add_argument('--max_grad_norm', default=40.0, type=float,
+                        help='Clip gradients to this norm')
+    parser.add_argument('--evaluation_interval', default=10, type=int,
+                        help='Evaluate and print results every x epochs')
+    parser.add_argument('--batch_size', default=32, type=int, help='Batch size for training')
+    parser.add_argument('--hops', default=3, type=int, help='Number of hops in the Memory Network')
+    parser.add_argument('--epochs', default=200, type=int, help='Number of epochs to train for')
+    parser.add_argument('--embedding_size', default=20, type=int,
+                        help='Embedding size for embedding matrices')
+    parser.add_argument('--memory_size', default=50, type=int, help='Maximum size of memory')
+    parser.add_argument('--task_id', default=6, type=int, help='bAbI task id, 1 <= id <= 6')
+    parser.add_argument('--random_state', default=None, help='Random state')
+    parser.add_argument('--data_dir', default='data/dialog-bAbI-tasks/',
+                        help='Directory containing bAbI tasks')
+    parser.add_argument('--model_dir', default='model/',
+                        help='Directory containing memn2n model checkpoints')
+    parser.add_argument('--train', default=1, type=int, help='Train if True, test if False')
+    parser.add_argument('--interactive', default=0, type=int, help='if True, interactive')
+    parser.add_argument('--OOV', default=0, type=int, help='if True, use OOV test set')
+    parser.add_argument('--print_params', default=1, type=int,
+                        help='pass False to turn off printing input parameters')
+    parser.add_argument('--save_model', default=5, type=int,
+                        help='Save model after every x epochs')
+    parser.add_argument('--checkpoint_path', default='./models',
+                        help='Path to the directory to save models')
+
+    args = parser.parse_args()
+    params = vars(args)  # convert to ordinary dict
+    if params['print_params']:
+        print('parsed input parameters:')
+        print(json.dumps(params, indent=2))
+
+main(params)
